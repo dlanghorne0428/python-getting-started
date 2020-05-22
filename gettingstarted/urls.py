ca9f71a60a94ff4672e350af135684c51748318a
David Langhorne <david.b.langhorne@gmail.com>
2020-05-22 19:45:12 -0400

    add celery-progress bar to app

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
-
+from django.urls import include, path, re_path
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
 
 admin.autodiscover()
 
@@ -15,7 +16,11 @@ import hello.views
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
 urlpatterns = [
+    re_path(r'^celery-progress/', include('celery_progress.urls')),  # the endpoint is configurabl
     path("", hello.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
+    path('progress', hello.views.progress_view, name="progress_view"),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- hello/views.py
+++ hello/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+from .tasks import my_task
 import os
 import requests
 
 # Create your views here.
 def index(request):
-    times = int(os.environ.get('TIMES',3))
-    return HttpResponse('Hello - World - whats up?? ' * times)
+    #times = int(os.environ.get('TIMES',3))
+    #return HttpResponse('Hello - World - whats up?? ' * times)
+    return render(request, "index.html")
 
 
 def db(request):
@@ -19,3 +21,8 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, "db.html", {"greetings": greetings})
+
+
+def progress_view(request):
+    result = my_task.delay(10)
+    return render(request, 'display_progress.html', context={'task_id': result.task_id})
